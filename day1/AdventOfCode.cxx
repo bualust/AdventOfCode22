47e815edb1be45afa88ac4b2c228bdb14155ecb5
Valentina Vecchio <valentina.vecchio12@gmail.com>
2022-12-01 19:49:17 +0000

    using namespace std

--- day1/AdventOfCode.cxx
+++ day1/AdventOfCode.cxx
@@ -4,37 +4,39 @@
 #include <fstream>
 #include <sstream>
 
-std::vector<int> get_elves_cal();
+using namespace std;
+
+vector<int> get_elves_cal();
 
 int main() {
 
-    std::vector<int> elves_cal = get_elves_cal();
+    vector<int> elves_cal = get_elves_cal();
 
     int number_of_elves = int(elves_cal.size());
-    std::cerr<<"Number of elves is "<<number_of_elves<<std::endl;
+    cerr<<"Number of elves is "<<number_of_elves<<endl;
 
-    std::sort(elves_cal.begin(), elves_cal.end());
+    sort(elves_cal.begin(), elves_cal.end());
     int max_calories = elves_cal.at(number_of_elves-1);
-    std::cerr<<"Max calories are "<<max_calories<<std::endl;
+    cerr<<"Max calories are "<<max_calories<<endl;
 
     int top_three = 0;
     for(int i = 0; i<3; i++){
         int index = number_of_elves-i-1;
         top_three+=elves_cal.at(index);
     }
-    std::cerr<<"Top three elves calories "<<top_three<<std::endl;
+    cerr<<"Top three elves calories "<<top_three<<endl;
     return 0;
 }
 
-std::vector<int> get_elves_cal(){
+vector<int> get_elves_cal(){
 
-    std::vector<int> out_elves_cal;
-    std::ifstream input; 
+    vector<int> out_elves_cal;
+    ifstream input; 
     input.open("input.txt");
     int calories = 0;
-    for( std::string line; getline( input, line ); ) {
+    for( string line; getline( input, line ); ) {
         if(!line.empty()) {
-            calories+=std::stoi(line);
+            calories+=stoi(line);
         } else {
             out_elves_cal.push_back(calories);
             calories=0;
